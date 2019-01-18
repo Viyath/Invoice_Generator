a4951f35a342f4726038348ef7b5d8f52ecf3145
vi <kvkanthony@gmail.com>
2019-01-19 10:07:27 +1100

    read db connection string from evnoirnment variable

--- config.js
+++ config.js
@@ -1,11 +1,7 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: 'my_invoice'
-});
+var connectionstring = process.env.JAWSDB_URL || 'mysql://root:@localhost:3306/my_invoice'
+var connection = mysql.createConnection(connectionstring);
 
 connection.connect(function(err){
     if(!err) {
@@ -14,4 +10,4 @@ connection.connect(function(err){
         console.log("Error while connecting with database");
     }
 });
-    module.exports = connection;
+module.exports = connection;
